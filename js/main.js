df329c489481503340cea20fae954be35dd3af5b
Ryan Malonzo <ryan@ryanmalonzo.fr>
2021-10-10 14:13:57 +0200

    Ajoute dropdown sur Services municipaux (navbar)

--- js/main.js
+++ js/main.js
@@ -1,19 +1,28 @@
 function changerFondNavbar() {
     let scroll = $(window).scrollTop();
     let navbar = $(".navbar");
+    let services = $(".dropdown-menu");
+
+    let blue = "#14679e";
+    let carouselBackground = getComputedStyle(document.documentElement).getPropertyValue("--carousel-background");
 
     if (!collapsed || scroll > 0) {
         // Fond bleu
         navbar.removeAttr("style");
-        navbar.css("background", "linear-gradient(to right, #10527E, #197bbd)");
+        services.removeAttr("style");
+        //navbar.css("background", "linear-gradient(to right, #10527E, #197bbd)");
+        navbar.css("background-color", blue);
         navbar.css("padding-top", "0");
         navbar.css("padding-bottom", "0");
+        services.css("background-color", blue);
     } else {
         // Fond transparent
         navbar.removeAttr("style");
+        services.removeAttr("style");
         navbar.css("background-color", "transparent");
         navbar.css("padding-top", "10px");
         navbar.css("padding-bottom", "10px");
+        services.css("background-color", carouselBackground);
     }
 }
 
