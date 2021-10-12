31b50442c1b3f7eac6733bdc8135ccb069df0214
Ryan Malonzo <ryan@ryanmalonzo.fr>
2021-10-13 01:07:00 +0200

    Optimisations de performance

--- cwebp.cmd
+++ cwebp.cmd
@@ -4,7 +4,6 @@ rem small
 cwebp.exe ./img/background.png -o .\build\img\320\background.webp -m 6 -q 50 -resize 320 0 -quiet
 cwebp.exe ./img/brehat.png -o .\build\img\320\brehat.webp -m 6 -q 50 -resize 320 0 -quiet
 cwebp.exe ./img/chapelle.jpg -o .\build\img\320\chapelle.webp -m 6 -q 50 -resize 320 0 -quiet
-cwebp.exe ./img/logo.png -o .\build\img\320\logo.webp -m 6 -q 50 -resize 320 0 -quiet
 cwebp.exe ./img/office.jpg -o .\build\img\320\office.webp -m 6 -q 50 -resize 320 0 -quiet
 cwebp.exe ./img/port.jpg -o .\build\img\320\port.webp -m 6 -q 50 -resize 320 0 -quiet
 
@@ -12,7 +11,6 @@ rem medium
 cwebp.exe ./img/background.png -o .\build\img\640\background.webp -m 6 -q 50 -resize 640 0 -quiet
 cwebp.exe ./img/brehat.png -o .\build\img\640\brehat.webp -m 6 -q 50 -resize 640 0 -quiet
 cwebp.exe ./img/chapelle.jpg -o .\build\img\640\chapelle.webp -m 6 -q 50 -resize 640 0 -quiet
-cwebp.exe ./img/logo.png -o .\build\img\640\logo.webp -m 6 -q 50 -resize 640 0 -quiet
 cwebp.exe ./img/office.jpg -o .\build\img\640\office.webp -m 6 -q 50 -resize 640 0 -quiet
 cwebp.exe ./img/port.jpg -o .\build\img\640\port.webp -m 6 -q 50 -resize 640 0 -quiet
 
@@ -20,6 +18,5 @@ rem large
 cwebp.exe ./img/background.png -o .\build\img\1024\background.webp -m 6 -q 50 -resize 1024 0 -quiet
 cwebp.exe ./img/brehat.png -o .\build\img\1024\brehat.webp -m 6 -q 50 -resize 1024 0 -quiet
 cwebp.exe ./img/chapelle.jpg -o .\build\img\1024\chapelle.webp -m 6 -q 50 -resize 1024 0 -quiet
-cwebp.exe ./img/logo.png -o .\build\img\1024\logo.webp -m 6 -q 50 -resize 1024 0 -quiet
 cwebp.exe ./img/office.jpg -o .\build\img\1024\office.webp -m 6 -q 50 -resize 1024 0 -quiet
 cwebp.exe ./img/port.jpg -o .\build\img\1024\port.webp -m 6 -q 50 -resize 1024 0 -quiet
